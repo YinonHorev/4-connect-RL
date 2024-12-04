483ff12bf3ce9e1f8f255543b69a4575feafbc10
Eva Imbens <e.imbens@gmail.com>
2024-12-04 20:24:03 +0100

    Fix MCTS algorithm

--- mcts.py
+++ mcts.py
@@ -7,21 +7,26 @@ import random
 
 
 def play():
+    '''
+    Play a game of Connect Four.
+    Player green uses the MCTS algorithm to determine the best move.
+    Player black chooses a random move.
+    '''
     state = GameState()
     state.render()
 
     while not (winner := state.is_terminal()):
-        # move = int(input(f"Player {state.current_player}, enter a move: "))
         if state.current_player == Players.green.value:
-            state = mcts(state, 100)
-            if winner := state.is_terminal():
-                break
+            move = mcts(state, 1000)
         else:
             move = random.choice(state.get_legal_moves())
-            state.make_move(move)
+        print(f"Player {state.current_player} chooses column {move}")
+        state.make_move(move)
         state.render()
+    
+    if winner == True:
+        print("Draw!")
     print(f"Player {winner} wins!")
-
 
 def assignment_board() -> np.ndarray:
     board = np.asarray(
@@ -36,18 +41,15 @@ def assignment_board() -> np.ndarray:
     )
     return board
 
-
-def mcts(state: GameState, n_simulations: int) -> GameState:
-    """
-    Monte Carlo Tree Search algorithm
-    """
-    root = Node(state, None)
-    for i in range(n_simulations):
+def mcts(state: GameState, n_simulations: int):
+    '''Determine the best move using the Monte Carlo Tree Search algorithm.'''
+    root = Node(state, None, None)
+    for _ in range(n_simulations):
         # Select nodes recursively until a leaf node is reached
         leaf = select(root)
         if winner := leaf.state.is_terminal():
             reward = get_reward(winner)
-            leaf = backtrack(leaf, reward)
+            backtrack(leaf, reward)
             continue
 
         # Expand the leaf node
@@ -58,45 +60,40 @@ def mcts(state: GameState, n_simulations: int) -> GameState:
 
         # Backtrack the result of the simulation
         leaf = backtrack(leaf, reward)
-    return root.best_child().state
 
+    best_move, _ = root.best_child()
+    return best_move
 
-def select(root: Node):
-    """
-    Select the most promising leaf based on UCB formula
-    """
+def select(root: Node) -> Node:
+    '''Select the most promising leaf based on UCB formula.'''
     curr_node = root
     while curr_node.children:
-        curr_node = curr_node.best_child()
+        _, curr_node = curr_node.best_child()
     return curr_node
 
-
-def simulate(node: Node):
+def simulate(node: Node) -> int:
+    '''Simulate a random game from the current state.'''
     temp_state = node.state.clone()
     while not (winner := temp_state.is_terminal()):
         move = random.choice(temp_state.get_legal_moves())
         temp_state.make_move(move)
     return get_reward(winner)
 
-
-def backtrack(node: Node, reward: int):
-    """
-    Update value and visits for states expanded in selection and expansion
-    """
-    while node := node.parent:
-        node.simulations += 1
-        node.wins += reward
+def backtrack(node: Node, reward: int) -> Node:
+    '''Update value and visits for states expanded in selection and expansion.'''
+    while node:
+        node.update(reward)
+        node = node.parent
     return node
 
-
-def get_reward(winner: Players | bool) -> int:
+def get_reward(winner: int) -> int:
+    '''Return the reward for the simulation.'''
     if winner == True:
         return 0
     elif winner == Players.green:
         return 1
     else:
         return -1
-
 
 if __name__ == "__main__":
     play()
--- node.py
+++ node.py
@@ -1,22 +1,48 @@
 from state import GameState
 import math
-from typing import Self
+
 
 EXPLOR_PAR = math.sqrt(2)
 
-
 class Node:
-    def __init__(self, state: GameState, parent: Self | None):
+    def __init__(self, state: GameState, parent: "Node", move: int):
         self.state = state
         self.parent = parent
         self.children = {}
         self.wins = 0
         self.simulations = 0
 
-    def best_child(self) -> Self:
-        return max(self.children.values(), key=lambda x: x.ucb())
+        if parent is not None:
+            parent.children[move] = self
 
-    def ucb(self) -> float:
+    def best_child(self) -> tuple[int, "Node"]:
+        '''Return the move and corresponding child node with the highest UCB value.'''
+        for move, child in self.children.items():
+            if child.simulations == 0:
+                return move, child
+        max_move, max_child = max(
+            self.children.items(),
+            key=lambda item: item[1]._ucb()
+        )
+        return max_move, max_child
+
+    def add_children(self):
+        '''Add all legal next moves as children to the node.'''
+        legal_moves = self.state.get_legal_moves()
+        if not legal_moves:
+            raise ValueError("No legal moves")
+        for move in legal_moves:
+            new_state = self.state.clone()
+            new_state.make_move(move)
+            self.children[move] = Node(new_state, self, move)
+
+    def update(self, result):
+        '''Update the win and simulation count of the node, based on the result of the simulation.'''
+        self.simulations += 1
+        self.wins += result
+
+    def _ucb(self) -> float:
+        '''Calculate the Upper Confidence Bound of the node.'''
         return self._exploitation() + self._exploration()
 
     def _exploitation(self) -> float:
@@ -28,18 +54,6 @@ class Node:
             if self.simulations != 0
             else 0.0
         )
-
-    def add_children(self):
-        legal_moves = self.state.get_legal_moves()
-        if not legal_moves:
-            raise ValueError("No legal moves")
-        for move in legal_moves:
-            new_state = self.state.clone()
-            new_state.make_move(move)
-            self.children[move] = Node(new_state, self)
-
-    def update(self, result):
-        pass
 
     def __repr__(self):
         return f"Node({self.state.board}, {self.wins}, {self.simulations})"
